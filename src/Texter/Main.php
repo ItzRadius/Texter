2dfa71ba1a8e4e17403b64eef8d95d960e525dfe
Jake <jakehcasto@aol.com>
2016-03-27 20:52:22 -0400

    Update Main.php

--- src/Texter/Main.php
+++ src/Texter/Main.php
@@ -33,11 +33,10 @@ class Main extends PluginBase implements Listener{
     
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
         if($cmd->getName() == "texter"){
-        $level = $sender->getLevel();
+        $position = new Vector3($sender->x, $sender->y + 0.5, $sender->z);
         $text = implode(" ", $args);
         $config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
 	$config->set($text, $position);
-        $position = new Vector3($sender->x, $sender->y + 0.5, $sender->z);
         $sender->getLevel()->addParticle(new FloatingTextParticle($position, $level, $text));
         }
 }
